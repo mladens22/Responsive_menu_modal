ad7b70ad6de233945092d4e91902e0df672d0d1e
mladens22 <mladjos75@gmail.com>
2022-05-13 20:47:36 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,103 @@
+//RESPONSIVE MENU
+
+const mobileMenu = () => {
+    let menu = document.querySelector('.header ul');
+    let btn = document.querySelector('.header button');
+
+    if (btn.innerText === 'MENU') {
+        menu.style.display = 'block';
+        btn.innerText = 'CLOSE';
+    } else {
+        menu.style.display = 'none';
+        btn.innerText = 'MENU';
+
+    }
+}
+
+// --------------------------------------------
+
+//SLIDER
+
+let rightBtn = document.querySelector('#right-btn');
+let leftBtn = document.querySelector('#left-btn');
+
+let pictures = document.querySelectorAll('.slider-images img');
+let imgNum = 0;
+
+const moveRight = () => {
+
+    displayNone(pictures);
+    imgNum++;
+
+    if (imgNum === pictures.length) {
+        imgNum = 0;
+    }
+
+    pictures[imgNum].style.display = 'block';
+}
+
+const moveLeft = () => {
+    displayNone();
+    imgNum--;
+
+    if (imgNum === -1) {
+        imgNum = pictures.length - 1;
+    }
+
+    pictures[imgNum].style.display = 'block';
+}
+
+rightBtn.addEventListener('click', moveRight)
+leftBtn.addEventListener('click', moveLeft)
+
+const displayNone = () => {
+    pictures.forEach(function (img) {
+        img.style.display = 'none';
+    })
+}
+
+//PORTFOLIO
+
+// ------------------------
+
+const portfolioSort = (button) => {
+    let category = button.getAttribute('data-category');
+    let portfolioItems = document.querySelectorAll('.portfolio-single-item'); //Kvadrati u kojima je tekst
+
+    portfolioItems.forEach(function (item) {
+        item.style.display = 'none';
+    })
+
+    if (category === 'sve') {
+        portfolioItems.forEach(function (item) {
+            item.style.display = 'block';
+        })
+    }
+
+    portfolioItems.forEach(function (item) {
+        if (item.getAttribute('data-category').includes(category)) {
+            item.style.display = 'block';
+        }
+    })
+}
+
+// -----------------------------------
+
+//MODAL BUTTON
+
+const openModal = () => {
+    let modalWindow = document.querySelector('.popup-modal');
+    let overlay = document.querySelector('.overlay');
+
+    modalWindow.style.display = 'block';
+    overlay.style.display = 'block';
+}
+
+const closeModal = () => {
+    let modalWindow = document.querySelector('.popup-modal');
+    let overlay = document.querySelector('.overlay');
+
+    modalWindow.style.display = 'none';
+    overlay.style.display = 'none';
+
+}
